16eeb0bd01c5f2567d388ebc10e1ba1fa37f6d72
mahulkko <martinhulkkonen@web.de>
2021-11-19 17:33:21 +0100

    Dash und Spark SQL Datenbank mit Testdaten

--- bigdata.py
+++ bigdata.py
@@ -10,7 +10,7 @@ import dash
 from dash import html
 from dash import dcc
 
-#import plotly.express as px
+import plotly.express as px
 import plotly.graph_objects as go
 
 
@@ -53,7 +53,7 @@ station_schema = sqlContext.read.options(delimiter=',').schema(station_schema).c
 
 # Import Gas Price from a day
 ################################################
-print("2. Laden von Spritpresien.csv")
+print("2. Laden von Spritpreisen.csv")
 gas_schema = StructType()
 gas_schema.add("date",TimestampType(),True)
 gas_schema.add("uuid",StringType(),True)
@@ -66,21 +66,41 @@ gas_schema.add("e10_change",StringType(),True)
 
 gas_schema = sqlContext.read.options(delimiter=',').schema(gas_schema).csv('2021-11-05-prices.csv')
 
-# gas_schema.show()
-# gas_schema.printSchema();
+gas_schema.show()
+gas_schema.printSchema();
+
+# Colect all Stations
+########################
+station_schema_collect = station_schema.collect()
+station_count = len(station_schema_collect)
+#station_count = 1000
 
 
+
+# Creat Arrays for Latitude, Longitute and Name
+##################################################
+lat_list = []
+lon_list = []
+name_list = []
+
+for i in range(1, station_count-1):
+    lon_list.append(float(station_schema_collect[i][8]))
+    lat_list.append(float(station_schema_collect[i][7]))
+    name_list.append(station_schema_collect[i][1])
+    
+    
 
 # Create MAP
 #################################################
 fig = go.Figure(go.Scattermapbox(
-        lat=['45.5017'],
-        lon=['-73.5673'],
+        lat=lat_list,
+        lon=lon_list,
         mode='markers',
         marker=go.scattermapbox.Marker(
-            size=14
+            size=8,
+            color='rgb(242, 177, 172)',
         ),
-        text=['Montreal'],
+        text=name_list,
     ))
 
 # Update MAP Layout
@@ -96,14 +116,16 @@ fig.update_layout(
        accesstoken=mapbox_access_token,
        bearing=0,
        center=go.layout.mapbox.Center(
-           lat=45,
-           lon=-73
+           lat=51,
+           lon=10
        ),
        pitch=0,
        zoom=5
    )
 )
 
+
+test_piv = gas_schema.filter(gas_schema.uuid == station_schema.collect()[1][0])
 
 # Define the Layout
 ################################################
@@ -123,7 +145,20 @@ app.layout = html.Div(
                        className='div-for-dropdown',
                        children=[
                         
-                           
+                           dcc.Graph(id='gas_price',
+                                                  config={'displayModeBar': False},
+                                                  animate=True,
+                                                  figure=px.line(test_piv.toPandas(),
+                                                                 x='date',
+                                                                 y=['diesel', 'e5', 'e10'],
+                                                                 template='plotly_dark').update_layout(
+                                                      {'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                                                       'paper_bgcolor': 'rgba(0, 0, 0, 0)'},
+                                                      yaxis_title='Preis',
+                                                      xaxis_title='Uhrzeit',
+                                                      legend_title='Benzinart',
+                                                      title=station_schema.collect()[1][1])
+                                                  )
                         
                        ],
                     style={'color': '#1E1E1E'})
@@ -137,7 +172,7 @@ app.layout = html.Div(
                     
                     # Show Map
                     ###################################                      
-                    dcc.Graph(id='timeseries', 
+                    dcc.Graph(id='gas_stations', 
                               style={'width': '100vh', 'height': '100vh'}, 
                               figure=fig
                 )
